c5d68e6aae4b067448f46ac8ea0204a3efd661aa
davay42 <fond@fondfrukt.ru>
2021-06-22 00:44:48 +0300

    clean theme

---
+++ .vitepress/theme/index.js
@@ -0,0 +1,10 @@
+import 'virtual:windi.css'
+
+import Layout from './layout.vue'
+import NotFound from './not-found.vue'
+
+export default {
+  Layout,
+  NotFound,
+  enhanceApp({ app }) {},
+}
---
+++ .vitepress/theme/layout.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+p hello
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+const props = defineProps({
+
+});
+</script>
+
+<style scoped>
+</style>
