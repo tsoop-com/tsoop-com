cf0e1822efa1e56197b18c70f56e0b65b1bd7fa8
davay42 <fond@fondfrukt.ru>
2021-06-22 13:24:16 +0300

    first deploy

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:tsoop/chromatone.center.git master:gh-pages
+git push -f git@github.com:tsoop-ru/tsoop.ru.git master:gh-pages
 
 cd -
