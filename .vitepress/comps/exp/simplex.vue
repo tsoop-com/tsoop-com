a4991d8f3f7abdabb9c4b1ea295fa67953369fa2
davay42 <fond@fondfrukt.ru>
2021-06-23 00:44:46 +0300

    simp

--- .vitepress/comps/exp/simplex.vue
+++ .vitepress/comps/exp/simplex.vue
@@ -1,19 +1,20 @@
 <template lang="pug">
-.flex.flex-col
-  input(type="range", v-model="z",min="0",max="200")
-  p {{ z }}
-  .flex.flex-col
-    .p-1.flex(v-for="y in 50", :key="y")
-      .p-1.m-1(
-        v-for="x in 50", 
+.flex.flex-col.items-center
+  .flex.flex-col.p-8
+    .flex.m-1px(v-for="y in 20", :key="y")
+      .p-4.m-1px.rounded(
+        v-for="x in 20", 
         :key="x", 
-        :style="{ backgroundColor: `hsla(0,0%,100%,${simplex.noise3D(y, x / 10, z / 100) / 2 + 0.5}` }"
+        :style="{ backgroundColor: `hsla(0,0%,100%,${simplex.noise3D(y / 5, x / 5, timestamp / 10000) / 2 + 0.5}` }"
         ) 
 </template>
 
 <script setup>
 import { defineProps, ref } from 'vue'
 import SimplexNoise from 'simplex-noise'
+import { useTimestamp } from '@vueuse/core'
+
+const timestamp = useTimestamp({ offset: -Date.now() })
 
 const z = ref(1)
 const simplex = new SimplexNoise()
