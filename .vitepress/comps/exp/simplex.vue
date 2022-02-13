bb0cc21adf695735c8e52f55b6744947a3aa1e5a
davay <fond@fondfrukt.ru>
2022-02-14 00:05:00 +0300

    bump

--- .vitepress/comps/exp/simplex.vue
+++ .vitepress/comps/exp/simplex.vue
@@ -12,70 +12,72 @@
       input(type="checkbox" id="polygon" v-model="show.polygon" )
       label.p-2(for="polygon") Polygons
     input(v-model="zoom" id="zoom" type="range" max="100" min="1" step="1")
-  svg.max-h-full.m-auto(
+  svg.max-h-100vh.m-auto(
     style="transform-box: fill-box"
     version="1.1",
     baseProfile="full",
     viewBox="-10 -10 100 100",
     xmlns="http://www.w3.org/2000/svg",
     )
+
     g(v-for="y in 20" :key="y")
-      rect(
-        v-if="show.box"
-        v-for="x in 20" 
-        :key="x"
-        :fill="`hsla(${simplex.noise3D(y / zoom, x / zoom, timestamp / speed) * 180},50%,50%,1)`"
-        :x="x * 4 - 4"
-        :y="y * 4 - 2"
-        rx="2"
-        ry="2"
-        width="4"
-        height="4"
-      )
-      line(
-        v-if="show.line"
-        v-for="x in 20" 
-        :key="x"
-        stroke-linecap="round"
-        stroke="white"
-        :transform-origin="`${x * 4 - 2}  ${y * 4}`"
-        stroke-width="0.4"
-        :x1="x * 4 - 2"
-        :x2="x * 4 - 2"
-        :y1="y * 4 - 1.5"
-        :y2="y * 4 + 1.5"
-        :transform="`rotate(${simplex.noise3D(y / zoom, x / zoom, timestamp / speed) * 180})`"
-      )
-      circle(
-        v-if="show.circle"
-        v-for="x in 20" 
-        :key="x"
-        :cx="x * 4 - 2"
-        :cy="y * 4 + 0.2"
-        fill="none"
-        stroke-width="0.1"
-        :stroke="`hsla(${simplex.noise3D(y / zoom, x / zoom, timestamp / speed) * 180},50%,50%,1)`"
-        :r="simplex.noise3D(y / zoom, x / zoom, timestamp / speed) * 4 + 4"
-      )
-    g(v-for="t in 3" :key="t")
-      polygon.fltr.mix-blend-multiply(
-        :points="getPoints(t)"
-        :fill="`hsla(${simplex.noise3D(t * t * 20, t * 20, timestamp / speed) * 180},50%,50%,1)`"
+      g(v-if="show.box")
+        rect(
+          v-for="x in 20" 
+          :key="x"
+          :fill="`hsla(${simplex.noise3D(y / zoom, x / zoom, timestamp / speed) * 180},50%,50%,1)`"
+          :x="x * 4 - 4"
+          :y="y * 4 - 2"
+          rx="2"
+          ry="2"
+          width="4"
+          height="4"
         )
-      line.mix-blend-difference(
-        stroke="white"
-        stroke-width="0.2"
-        :x1="simplex.noise3D(t + 5, t + 5, timestamp / speed) * 40 + 40"
-        :y1="simplex.noise3D(t + 10, t + 10, timestamp / speed) * 40 + 40"
-        :x2="simplex.noise3D(t + 20, t + 20, timestamp / speed) * 40 + 40"
-        :y2="simplex.noise3D(t + 30, t + 30, timestamp / speed) * 40 + 40"
-      )
-      circle.mix-blend-screen(
-        :fill="`hsla(${simplex.noise3D(t * t, t + 100, timestamp / speed) * 180},50%,50%,1)`"
-        :cx="simplex.noise3D(t + t * 20, t + 20, timestamp / speed) * 30 + 40"
-        :cy="simplex.noise3D(t * t + 200, t - 10, timestamp / speed) * 30 + 40"
-        :r="simplex.noise3D(t * 20, t + 220, timestamp / speed) * 10 + 15"
-      )
+      g(v-if="show.line")
+        line(
+          v-for="x in 20" 
+          :key="x"
+          stroke-linecap="round"
+          stroke="white"
+          :transform-origin="`${x * 4 - 2}  ${y * 4}`"
+          stroke-width="0.4"
+          :x1="x * 4 - 2"
+          :x2="x * 4 - 2"
+          :y1="y * 4 - 1.5"
+          :y2="y * 4 + 1.5"
+          :transform="`rotate(${simplex.noise3D(y / zoom, x / zoom, timestamp / speed) * 180})`"
+        )
+      g(v-if="show.circle")
+        circle(
+          v-for="x in 20" 
+          :key="x"
+          :cx="x * 4 - 2"
+          :cy="y * 4 + 0.2"
+          fill="none"
+          stroke-width="0.1"
+          :stroke="`hsla(${simplex.noise3D(y / zoom, x / zoom, timestamp / speed) * 180},50%,50%,1)`"
+          :r="simplex.noise3D(y / zoom, x / zoom, timestamp / speed) * 4 + 4"
+        )
+    g(v-if="show.polygon")
+      g(v-for="t in 3" :key="t")
+        polygon.fltr.mix-blend-multiply(
+          :points="getPoints(t)"
+          :fill="`hsla(${simplex.noise3D(t * t * 20, t * 20, timestamp / speed) * 180},50%,50%,1)`"
+          )
+        line.mix-blend-difference(
+          stroke="white"
+          stroke-width="0.2"
+          :x1="simplex.noise3D(t + 5, t + 5, timestamp / speed) * 40 + 40"
+          :y1="simplex.noise3D(t + 10, t + 10, timestamp / speed) * 40 + 40"
+          :x2="simplex.noise3D(t + 20, t + 20, timestamp / speed) * 40 + 40"
+          :y2="simplex.noise3D(t + 30, t + 30, timestamp / speed) * 40 + 40"
+        )
+        circle.mix-blend-screen(
+          :fill="`hsla(${simplex.noise3D(t * t, t + 100, timestamp / speed) * 180},50%,50%,1)`"
+          :cx="simplex.noise3D(t + t * 20, t + 20, timestamp / speed) * 30 + 40"
+          :cy="simplex.noise3D(t * t + 200, t - 10, timestamp / speed) * 30 + 40"
+          :r="simplex.noise3D(t * 20, t + 220, timestamp / speed) * 10 + 15"
+        )
 </template>
 
 <script setup>
