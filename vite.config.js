f14e22d2b11810aa52c319948fb67bb086e45596
davay <fond@fondfrukt.ru>
2022-09-16 01:13:00 +0300

    bump all

--- vite.config.js
+++ vite.config.js
@@ -4,63 +4,59 @@ import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 import WindiCSS from 'vite-plugin-windicss'
 import { ViteAliases } from 'vite-aliases'
-import glsl from 'vite-plugin-glsl'
 import AutoImport from 'unplugin-auto-import/vite'
 
-export default defineConfig({
-  plugins: [
-    AutoImport({
-      // targets to transform
-      include: [
-        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
-        /\.vue\??/, // .vue
-      ],
-      imports: ['vue'],
-    }),
-    glsl(),
-    ViteAliases({
-      dir: '.vitepress',
-      deep: false,
-      adjustDuplicates: true,
-    }),
-    Components({
-      dirs: ['.vitepress/theme/components', '.vitepress/comps'],
-      extensions: ['vue', 'ts'],
-      directoryAsNamespace: true,
-      globalNamespaces: ['global'],
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      exclude: [/node_modules/, /\.git/],
-      resolvers: [
-        IconsResolver({
-          componentPrefix: '',
-        }),
-      ],
-    }),
-    Icons({
-      defaultStyle: 'vertical-align: middle;',
-    }),
-    WindiCSS({
-      scan: {
-        dirs: ['.vitepress'],
-        include: ['index.md'],
-        exclude: ['**/examples/**/*'],
-        fileExtensions: ['vue', 'ts'],
-      },
-    }),
-  ],
-  optimizeDeps: {
-    include: ['vue', '@vueuse/core', 'three', 'gsap'],
-  },
-  ssr: {
-    noExternal: ['hydra-ts']
-  },
-  build: {
-    chunkSizeWarningLimit: 800,
-    rollupOptions: {
-      output: {
-        manualChunks: {
+export default defineConfig(async () => {
+  const glsl = (await import('vite-plugin-glsl')).default;
+  return {
+    plugins: [
+      AutoImport({
+        // targets to transform
+        include: [
+          /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+          /\.vue\??/, // .vue
+        ],
+        imports: ['vue'],
+      }),
+      glsl(),
+      ViteAliases({
+        dir: '.vitepress',
+        deep: false,
+        adjustDuplicates: true,
+      }),
+      Components({
+        dirs: ['.vitepress/theme/components', '.vitepress/comps'],
+        extensions: ['vue', 'ts'],
+        directoryAsNamespace: true,
+        globalNamespaces: ['global'],
+        include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+        exclude: [/node_modules/, /\.git/],
+        resolvers: [
+          IconsResolver({
+            componentPrefix: '',
+          }),
+        ],
+      }),
+      Icons({
+        defaultStyle: 'vertical-align: middle;',
+      }),
+      WindiCSS({
+        scan: {
+          dirs: ['.vitepress'],
+          include: ['index.md'],
+          exclude: ['**/examples/**/*'],
+          fileExtensions: ['vue', 'ts'],
         },
-      },
+      }),
+    ],
+    optimizeDeps: {
+      include: ['vue', '@vueuse/core', 'three', 'gsap'],
     },
-  },
+    ssr: {
+      noExternal: ['hydra-ts']
+    },
+    build: {
+      chunkSizeWarningLimit: 800,
+    },
+  }
 })
