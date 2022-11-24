7e76ccc2e0612662050fa419a5b37c7b7e74d536
davay <fond@fondfrukt.ru>
2022-11-25 01:09:50 +0700

    bump

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import Components from 'unplugin-vue-components/vite'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 import WindiCSS from 'vite-plugin-windicss'
-import { ViteAliases } from 'vite-aliases'
 import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig(async () => {
@@ -19,11 +18,6 @@ export default defineConfig(async () => {
         imports: ['vue'],
       }),
       glsl(),
-      ViteAliases({
-        dir: '.vitepress',
-        deep: false,
-        adjustDuplicates: true,
-      }),
       Components({
         dirs: ['.vitepress/theme/components', '.vitepress/comps'],
         extensions: ['vue', 'ts'],
