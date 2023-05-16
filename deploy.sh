dfca992f3693cb991f436e0b48893a4218761965
davay <davay@defucc.me>
2023-05-16 12:57:45 +0700

    tsoop-com

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:tsoop-ru/tsoop.ru.git master:gh-pages
+git push -f git@github.com:tsoop-com/tsoop-com.git master:gh-pages
 
 cd -
