ef34a075b21213e3dadd019567e50217d1576e70
davay <davay@defucc.me>
2023-06-14 13:38:08 +0700

    deploy

--- assets/chunks/ExpShader.5b746373.js
+++ assets/chunks/ExpShader.5b746373.js
@@ -1,4 +1,4 @@
-import{c as h}from"./index.562d0107.js";import{r as l,j as s,C as e,o as x,c as y,e as o,D as i}from"./framework.9816b6c1.js";import"./index.be8cf4d5.js";const C=`vec3 hsv2rgb(vec3 c) {
+import{c as h,T as x}from"./index.1169a3bc.js";import{r as l,j as s,C as e,o as y,c as C,e as o,D as i}from"./framework.9816b6c1.js";import"./index.be8cf4d5.js";const w=`vec3 hsv2rgb(vec3 c) {
   vec4 K = vec4(1., 2. / 3., 1. / 3., 3.);
   vec3 p = abs(fract(c.xxx + K.xyz) * 6. - K.www);
   return c.z * mix(K.xxx, clamp(p - K.xxx, 0., 1.), c.y);
@@ -44,9 +44,9 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
 
     vec3 color = hsv2rgb(vec3(i / 12., velocity, .8));
 
-    vec4 layer2 = circle(uv, position, radius, color);
+    vec4 layer2 = circle(uv, position, radius * velocity + 1., color);
 
     layer1 = mix(layer1, layer2, layer2.a);
   }
   fragColor = layer1;
-}`,M={__name:"ExpShader",setup(w){const c=l(0),v=s(()=>C),a=l(Array.from({length:12},()=>Math.random())),u=h(a),m=s(()=>{const n=Array(16).fill(0);return u.value.forEach((r,t)=>n[t]=r),n});function p(n){c.value=(Math.sin(n.iTime)+1)/2}function f(){a.value=a.value.map(n=>Math.random())}return(n,r)=>{const t=e("gl-float"),d=e("gl-mat4"),_=e("gl-program"),g=e("gl-canvas");return x(),y("div",{class:"min-h-80vh h-100vh",onClick:f},[o(g,{onUpdate:p},{default:i(()=>[o(_,{name:"main",code:v.value},{default:i(()=>[o(t,{name:"u_light",value:c.value},null,8,["value"]),o(d,{name:"u_notes",value:m.value},null,8,["value"])]),_:1},8,["code"])]),_:1})])}}};export{M as default};
+}`,E={__name:"ExpShader",setup(z){const r=l(0),v=s(()=>w),a=l(Array.from({length:12},()=>Math.random())),u=h(a,{duration:200,transition:x.easeInOutExpo}),m=s(()=>{const n=Array(16).fill(0);return u.value.forEach((c,t)=>n[t]=c),n});function p(n){r.value=(Math.sin(n.iTime)+1)/2}function f(){a.value=a.value.map(n=>Math.random())}return(n,c)=>{const t=e("gl-float"),d=e("gl-mat4"),_=e("gl-program"),g=e("gl-canvas");return y(),C("div",{class:"min-h-80vh h-100vh",onClick:f},[o(g,{onUpdate:p},{default:i(()=>[o(_,{name:"main",code:v.value},{default:i(()=>[o(t,{name:"u_light",value:r.value},null,8,["value"]),o(d,{name:"u_notes",value:m.value},null,8,["value"])]),_:1},8,["code"])]),_:1})])}}};export{E as default};
