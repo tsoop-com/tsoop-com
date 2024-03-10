4355b1f7ef6d0b0772484407f4d934c0d425ac3c
davay <davay@defucc.me>
2024-03-10 23:24:49 +0700

    deploy

---
+++ assets/experiments_shader_index.md.8UL4paBM.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/chunks/ExpShader.5LFus9h1.js","assets/chunks/framework.IrjygRV3.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{o as a,a as s,l as r,j as n,I as i,y as d}from"./chunks/framework.IrjygRV3.js";const _=JSON.parse('{"title":"GPU lights","description":"GLSL shader display","frontmatter":{"title":"GPU lights","description":"GLSL shader display","date":"2023-06-18T00:00:00.000Z","cover":"cover.png","links":["https://www.youtube.com/watch?v=f4s1h2YETNY"]},"headers":[],"relativePath":"experiments/shader/index.md","filePath":"experiments/shader/index.md","lastUpdated":1692861782000}'),o={name:"experiments/shader/index.md"},h=Object.assign(o,{setup(p){const t=i(()=>d(()=>import("./chunks/ExpShader.5LFus9h1.js").then(e=>e.E),__vite__mapDeps([0,1])));return(e,l)=>(a(),s("div",null,[r(n(t))]))}});export{_ as __pageData,h as default};
--- assets/experiments_shader_index.md.qpK6lLWp.lean.js
+++
@@ -1,7 +0,0 @@
-import{o as a,c as s,j as r,e as n,l as i,m as d}from"./chunks/framework.yTxprVqE.js";const _=JSON.parse('{"title":"GPU lights","description":"GLSL shader display","frontmatter":{"title":"GPU lights","description":"GLSL shader display","date":"2023-06-18T00:00:00.000Z","cover":"cover.png","links":["https://www.youtube.com/watch?v=f4s1h2YETNY"]},"headers":[],"relativePath":"experiments/shader/index.md","filePath":"experiments/shader/index.md","lastUpdated":1692861782000}'),o={name:"experiments/shader/index.md"},h=Object.assign(o,{setup(p){const t=i(()=>d(()=>import("./chunks/ExpShader.t0ZemKhU.js").then(e=>e.E),__vite__mapDeps([0,1])));return(e,c)=>(a(),s("div",null,[r(n(t))]))}});export{_ as __pageData,h as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/chunks/ExpShader.t0ZemKhU.js","assets/chunks/framework.yTxprVqE.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
