0b33b9b20b55050817f6f1102f501013a00f866d
davay42 <davay42@users.noreply.github.com>
2024-03-10 16:27:01 +0000

    deploy: 8ffc0d7b1d4bb70c28686e5a16b73cbe1c096611

---
+++ assets/experiments_shader_index.md.zs3UQ4Hs.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/chunks/ExpShader.fJ4t4GZ4.js","assets/chunks/framework.0tUUz2it.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{o as a,a as s,l as r,j as n,I as i,y as d}from"./chunks/framework.0tUUz2it.js";const _=JSON.parse('{"title":"GPU lights","description":"GLSL shader display","frontmatter":{"title":"GPU lights","description":"GLSL shader display","date":"2023-06-18T00:00:00.000Z","cover":"cover.png","links":["https://www.youtube.com/watch?v=f4s1h2YETNY"]},"headers":[],"relativePath":"experiments/shader/index.md","filePath":"experiments/shader/index.md","lastUpdated":1710087975000}'),o={name:"experiments/shader/index.md"},h=Object.assign(o,{setup(p){const t=i(()=>d(()=>import("./chunks/ExpShader.fJ4t4GZ4.js").then(e=>e.E),__vite__mapDeps([0,1])));return(e,l)=>(a(),s("div",null,[r(n(t))]))}});export{_ as __pageData,h as default};
